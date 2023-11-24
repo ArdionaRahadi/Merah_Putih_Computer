9b7e57539abe869d61a74548b75410c1409355e1
ardiona <ardionarahadi@gmail.com>
2023-11-24 18:47:15 +0700

    mengubah js

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,36 @@
 // validasi form
 function validasi() {
-  let nama = document.getElementById('nama').value;
-  let telp = document.getElementById('no').value;
-  let pesan = document.getElementById('pesan').value;
-  let URL = window.open('https://wa.me/6285606775439?text=*Nama* :%20' + nama + '%0a*No.Telp* :%20' + telp + '%0a*Pesan* :%20' + pesan + '%0a');
+  let nama = document.getElementById("nama").value;
+  let telp = document.getElementById("no").value;
+  let pesan = document.getElementById("pesan").value;
+  let URL = window.open(
+    "https://wa.me/6285606775439?text=*Nama* :%20" +
+      nama +
+      "%0a*No.Telp* :%20" +
+      telp +
+      "%0a*Pesan* :%20" +
+      pesan +
+      "%0a"
+  );
 
-  window.open(URL, '_blank') + Form.reset();
+  window.open(URL, "_top") + Form.reset();
 }
 
 // klik button untuk lihat lebih lanjut
-let box = document.getElementById('kotak');
-let btn1 = document.getElementById('seen');
-let btn2 = document.getElementById('hide');
+let box = document.getElementById("kotak");
+let btn1 = document.getElementById("seen");
+let btn2 = document.getElementById("hide");
 
 function seen() {
-  box.style.display = 'block';
-  btn1.style.display = 'none';
-  btn2.style.display = 'block';
+  box.style.display = "block";
+  btn1.style.display = "none";
+  btn2.style.display = "block";
 }
 
 function hide() {
-  box.style.display = 'none';
-  btn1.style.display = 'block';
-  btn2.style.display = 'none';
+  box.style.display = "none";
+  btn1.style.display = "block";
+  btn2.style.display = "none";
 }
 
 //
