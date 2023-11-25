f36275bc6ee27d792ecbd7b644c7936542a6039d
ardiona <ardionarahadi@gmail.com>
2023-11-25 19:08:35 +0700

    p

--- JS/script.js
+++ JS/script.js
@@ -15,15 +15,6 @@ function validasi() {
   );
 
   window.open(URL, "_top") + Form.reset();
-=======
-  let nama = document.getElementById('nama').value;
-  let telp = document.getElementById('no').value;
-  let pesan = document.getElementById('pesan').value;
-  let URL = window.open("https://api.whatsapp.com/send?phone=6285606775439&text=*Nama* :%20' + nama + '%0a*No.Telp* :%20' + telp + '%0a*Pesan* :%20' + pesan + '%0a");
-
-  window.open(URL, "_top");
->>>>>>> accc3fa6242e74e2098c7b8e3064473a440f3492
-}
 
 // klik button untuk lihat lebih lanjut
 let box = document.getElementById("kotak");
