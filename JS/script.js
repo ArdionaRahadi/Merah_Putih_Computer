990d8a0e0711d769a279b2e3571a7ee8df9eec0c
ardiona <ardionarahadi@gmail.com>
2024-01-04 19:49:54 +0700

    menonaktifkan fittur pencarian

--- JS/script.js
+++ JS/script.js
@@ -34,26 +34,22 @@ function hide() {
 }
 
 // Search Engine
-const search = () => {
-  const searchbox = document.getElementById("search-item").value.toLowerCase();
-  const storeitems = document.querySelectorAll("#product-list");
-  const product = document.querySelectorAll(".product");
-  const pname = document.querySelectorAll(".card-text");
+// const search = () => {
+//   const searchbox = document.getElementById("search-item").value.toLowerCase();
+//   const storeitems = document.querySelectorAll("#product-list");
+//   const product = document.querySelectorAll(".product");
+//   const pname = document.querySelectorAll(".card-text");
 
-  for (var i = 0; i < pname.length; i++) {
-    let match = product[i].querySelectorAll(".card-text")[0];
+//   for (var i = 0; i < pname.length; i++) {
+//     let match = product[i].querySelectorAll(".card-text")[0];
 
-    if (match) {
-      let textvalue = match.textContent || match.innerHTML;
+//     if (match) {
+//       let textvalue = match.textContent || match.innerHTML;
 
-      if (textvalue.toLowerCase().indexOf(searchbox) > -1) {
-        product[i].style.display = "";
-        box.style.display = "block";
-        btn1.style.display = "none";
-        btn2.style.display = "none";
-      } else {
-        product[i].style.display = "none";
-      }
-    }
-  }
-};
+//       if (textvalue.toLowerCase().indexOf(searchbox) > -1) {
+//         product[i].style.display = "";
+//       } else {
+//         product[i].style.display = "none";
+//       }
+//   }
+// };
